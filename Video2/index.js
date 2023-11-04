cc200a16d3fdfdc7fc85324aacde50cb26e6b614
YashSomani18 <yashsomani18@gmail.com>
2023-11-04 17:08:22 +0530

    Async Js

--- Video2/index.js
+++ Video2/index.js
@@ -1,2 +1,15 @@
-console.log("Video 2 starts now");
+// console.log("Video 2 starts now");
+
+
+// console.log(myFunc);
+// var myFunc = function (){
+//     console.log("This is function");
+// }
+// console.log(myFunc);
+
+
+
+console.log(first);
+var first = "Yash";
+console.log(first);
 
